a812aebd1e00c88393da2609209ae66becba0678
User <User@CIC001401>
2025-05-15 11:21:22 +0100

    Add project files.

---
+++ Omnia.DT4.Prosodic.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Omnia.DT4.Prosodic.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
