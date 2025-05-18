ba724faac2e9a0534c3c012616c462b6c0d2e253
Ken Eltringham <ken.eltringham@digital.mod.uk>
2025-05-18 20:25:36 +0100

    Init the App with an API

---
+++ Omnia.DT4.Prosodic.App/Controllers/ProsodicController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Omnia.DT4.Prosodic.App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProsodicController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { message = "Hello from API! - Under Construction" });
+        }
+    }
+}
